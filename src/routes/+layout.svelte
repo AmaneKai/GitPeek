2cd15d88b5cacddd52263a4197d5a4277369b717
Floranaras <carlosranara0@gmail.com>
2026-04-09 10:36:10 +0800

    feat(routes): add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "../app.css"
+  import { Toaster } from "svelte-sonner"
 
-	let { children } = $props();
+  let { children } = $props()
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<Toaster
+  position="bottom-right"
+  toastOptions={{
+    style: "background: #26233a; border: 1px solid rgba(144,140,170,0.25); color: #e0def4; font-family: 'DM Mono', monospace; font-size: 13px; border-radius: 14px;",
+  }}
+/>
+
 {@render children()}
