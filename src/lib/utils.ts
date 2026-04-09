48f54233fd31acafebe33169af6e4f776175e9fb
Floranaras <carlosranara0@gmail.com>
2026-04-09 16:44:05 +0800

    feat: added WithElementRef

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { ComponentType, SvelteComponent } from "svelte";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export type WithElementRef<T, TElement extends SvelteComponent = SvelteComponent> = T & {
+  ref?: ComponentType<TElement>;
+};
